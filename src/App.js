1231fd2dd23c97911a27948a03f8b6ed56412a8f
Sandu Abeywickrama <s@abey.one>
2023-06-10 10:13:41 +1000

    card modal added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import './App.css'
 import Header from './components/Layout/Header'
 import Meals from './components/Meals/Meals'
+import Cart from './components/Cart/Cart'
 
-import { Fragment } from 'react'
+import { Fragment, useState } from 'react'
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false)
+
+  const showCartHandler = () => {
+    setCartIsShown(true)
+  }
+
+  const hideCardHandler = () => {
+    setCartIsShown(false)
+  }
+
   return (
     <Fragment>
-      <Header></Header>
+      {cartIsShown && <Cart onClose={hideCardHandler} />}
+      <Header onShowCart={showCartHandler}></Header>
       <main>
         <Meals></Meals>
       </main>
